2f8bae555e536dbd1a22bb8ad61dc54b9dc7a789
tjens23 <tjens23@student.sdu.dk>
2025-03-14 13:53:09 +0100

    refactor: update GetHello function to return JSON response

--- src/controllers/AuthenticationController.go
+++ src/controllers/AuthenticationController.go
@@ -3,5 +3,8 @@ package controllers
 import "github.com/gofiber/fiber/v2"
 
 func GetHello(c *fiber.Ctx) error {
-	return c.SendString("Hello, World!")
+	return c.JSON(fiber.Map{
+		"message": "Hello, World!",
+		"status":  fiber.StatusOK,
+	})
 }
